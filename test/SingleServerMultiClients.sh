5824b80e06da42e7c60c19358c8ff9bff84328cc
Vignesh R <vigneshr@Vigneshs-MacBook-Pro.local>
2021-04-05 21:30:59 -0400

    INcreased sleep

--- test/SingleServerMultiClients.sh
+++ test/SingleServerMultiClients.sh
@@ -97,7 +97,7 @@ echo "Current count of the - $countBefore"
 echo "Attempting to buy this book twice concurrently."
 python3 src/cli/main.py localhost buy 2 &
 python3 src/cli/main.py localhost buy 2 &
-sleep 3
+sleep 5
 echo "Fetching the count after concurrent buys."
 tmpoutput=$(python3 src/cli/main.py localhost lookup 2)
 if [[ $tmpoutput == *"Failed"* ]] ; then
