6b55d04caa305bdead078bdf54d986370d9460c3
Vignesh R <vigneshr@Vigneshs-MacBook-Pro.local>
2021-04-23 20:51:54 -0400

    Made run.sh more robust by grepping for container name

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ servers=("catalog_server" "order_server" "frontend_server")
 
 function finish {
   echo "Cleanup"
-  for i in "${local_container_ids[@]}"
+  for i in "${local_container_names[@]}"
   do
    docker stop $i && docker rm $i || echo "Failed to kill container $i. It is probably already killed or something went wrong."
   done
@@ -17,7 +17,7 @@ function finish {
       ip=${ips[j]}  
       if [[ "$ip" != *"host.docker.internal" ]]
       then
-        echo "Attempting to cleanup ${servers[$i]} with container id ${container_ids[$i]} on $ip"
+        echo "Attempting to cleanup remote server $ip"
         ssh -n ec2-user@"$ip" "docker stop $(docker ps -aq) && docker rm $(docker ps -aq)" || echo "Failed to stop the server $i. It might have already been cleaned before or something went wrong."
       fi
     done
@@ -30,9 +30,16 @@ trap finish INT
 
 ports=("5000" "5001" "5002")
 machines=()
-local_container_ids=()
-container_ids=()
-configFile=$1
+local_container_names=()
+container_names=()
+ if [[ "$1" == "" ]]
+ then
+  echo "No file passed as parameter. Using the default localhost file \"machines.txt.local\"."
+  configFile="machines.txt.local"
+ else
+  configFile=$1
+ fi
+frontend_port=""
  # Read N - Number of servers. Keep assigning nodes to them - Catalog, Order, and Frontend and run the app accordingly. 
  # Print the url of frontend server. 
  # Run the client and test cases.
@@ -47,46 +54,53 @@ for i in ${!servers[@]}; do
   port_offset=${ports[$i]}
   for j in ${!ips[@]}; do
     port=$((port_offset+j*3))
-    echo $port
     ip=${ips[j]}
-    if [[ "$ip" == *"host.docker.internal" ]] || [[ "$ip" == *"127.0.0.1" ]]
+    container_name=${role}_$j
+    if [[ "$ip" == *"host.docker.internal" ]]
     then
       echo "Running $role on Localhost...."
       docker load < docker/${role}.tar.gz
+      
       if [[ "$role" == "frontend_server" ]]
       then
-        container_id=$(docker run -v $(pwd)/$configFile:/app/config -d -p $port:$port $role --host=0.0.0.0 --port $port)
+        frontend_port=$port
+        docker run -v $(pwd)/$configFile:/app/config -d -p $port:$port --name $container_name $role --host=0.0.0.0 --port $port
       else
-        container_id=$(docker run -v $(pwd)/$configFile:/app/config -d -p $port:$port $role $j)
+        docker run -v $(pwd)/$configFile:/app/config -d -p $port:$port --name $container_name $role $j
       fi
       sleep 3
       status=0
-      docker ps | grep $role | grep "${container_id:0:10}" | grep -v grep >/dev/null 2>&1 || status=$?
-      local_container_ids+=($container_id)
-      else
-        echo "Running role $role on remote machine $ip."
-        dir[$i]="temp_$i"
-        ssh -n ec2-user@"$ip" "rm -rf temp_$i && mkdir temp_$i && cd temp_$i && git clone https://github.com/CS677-Labs/Lab-3-Pygmy-The-book-store 1>/dev/null 2>&1  || echo \"Repo already present\""
-        scp $configFile  ec2-user@"$ip":"temp_$i/Lab-3-Pygmy-The-book-store/src/$role/config" >/dev/null 2>&1
-        if [[ "$role" == "frontend_server" ]]
-        then
-            container_id=$(ssh -n ec2-user@$ip "cd temp_$i/Lab-3-Pygmy-The-book-store && docker load docker/${role}.tar.gz && docker cp -f $configFile ${role}:/config && (docker run -v $(pwd)/$configFile:/app/config -d -p $port:$port $role --host=0.0.0.0 --port $port && echo \$!)")
-        else
-            container_id=$(ssh -n ec2-user@$ip "cd temp_$i/Lab-3-Pygmy-The-book-store && docker load docker/${role}.tar.gz && docker cp -f $configFile ${role}:/config && (docker run -v $(pwd)/$configFile:/app/config -d -p $port:$port $role $j && echo \$!)")
-        fi
-        sleep 3
-        status=0
-        ssh -n ec2-user@"$ip" "docker ps | grep $role | grep -v grep >/dev/null 2>&1" || status=$?
-        container_ids[i]=$container_id
-      fi
-      if [[ "$status" != 0 ]]
+      docker ps | grep "$container_name" | grep -v grep >/dev/null 2>&1 || status=$?
+      local_container_names+=($container_name)
+    else
+      echo "Running role $role on remote machine $ip."
+      dir[$i]="temp_$i"
+      ssh -n ec2-user@"$ip" "rm -rf temp_$i && mkdir temp_$i && cd temp_$i && git clone https://github.com/CS677-Labs/Lab-3-Pygmy-The-book-store 1>/dev/null 2>&1  || echo \"Repo already present\""
+      scp $configFile  ec2-user@"$ip":"temp_$i/Lab-3-Pygmy-The-book-store/src/$role/config" >/dev/null 2>&1
+      if [[ "$role" == "frontend_server" ]]
       then
-        echo "Failed to start the server $role in machine with ip $ip. Exiting..." && exit 1
+          ssh -n ec2-user@$ip "cd temp_$i/Lab-3-Pygmy-The-book-store && docker load docker/${role}.tar.gz && docker cp -f $configFile ${role}:/config && (docker run -v $(pwd)/$configFile:/app/config -d -p $port:$port --name $container_name $role --host=0.0.0.0 --port $port"
+      else
+          ssh -n ec2-user@$ip "cd temp_$i/Lab-3-Pygmy-The-book-store && docker load docker/${role}.tar.gz && docker cp -f $configFile ${role}:/config && (docker run -v $(pwd)/$configFile:/app/config -d -p $port:$port --name $container_name $role $j"
       fi
+      sleep 3
+      status=0
+      ssh -n ec2-user@"$ip" "docker ps | grep $container_name | grep -v grep >/dev/null 2>&1" || status=$?
+      container_names[i]=$container_name
+    fi
+    if [[ "$status" != 0 ]]
+    then
+      echo "Failed to start the server $role in machine with ip $ip. Exiting..." && exit 1
+    fi
   done
 done
 
-echo "Frontend server is running on ${machines[2]}.... Pass this to the CLI to use it with this server."
+if [[ "$ip" == *"host.docker.internal" ]]
+then
+  echo "Frontend server is running on http://localhost:$frontend_port .... Pass this to the CLI to use it with this server."
+else
+  echo "Frontend server is running on ${machines[2]}.... Pass this to the CLI to use it with this server."
+fi
 
 echo "Press 'q' to exit"
 count=0
